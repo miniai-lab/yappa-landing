90b3fc908c9b69d9a151c769f9ba3f04e8a9067f
Denis Naumov <mrsimaregele@gmail.com>
2025-01-11 01:19:17 +0400

    MailChimp форму добавил как попап

---
+++ js/main.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const downloadButtons = document.querySelectorAll('.mailchimp-trigger');
+    const customPopup = document.getElementById('customPopup');
+    const closePopup = document.querySelector('.close-popup');
+    
+    // Открытие попапа
+    downloadButtons.forEach(button => {
+        button.addEventListener('click', (e) => {
+            e.preventDefault();
+            customPopup.style.display = 'flex';
+            document.body.style.overflow = 'hidden';
+        });
+    });
+    
+    // Закрытие попапа при клике на крестик
+    closePopup.addEventListener('click', () => {
+        customPopup.style.display = 'none';
+        document.body.style.overflow = 'auto';
+    });
+    
+    // Закрытие попапа при клике вне формы
+    customPopup.addEventListener('click', (e) => {
+        if (e.target === customPopup) {
+            customPopup.style.display = 'none';
+            document.body.style.overflow = 'auto';
+        }
+    });
+});
